f55e4da75605e35d1891490d17028ed399ef8e0e
SHAHMEER AFTAB <154751468+aiwithshahmeer@users.noreply.github.com>
2024-02-28 15:49:39 +0500

    Add files via upload

---
+++ famous_quote02.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = 'Albert Einstien';
+let message = '"A person who never made a mistake never tired anything new"';
+console.log(`${famous_person} once said, ${message}`);
---
+++ famous_quote02.ts
@@ -0,0 +1,5 @@
+let famous_person : string = 'Albert Einstien';
+
+let message : string = '"A person who never made a mistake never tired anything new"';
+
+console.log(`${famous_person} once said, ${message}`);
